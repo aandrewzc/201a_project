06adf5f8c06e5d70b124c0aae083e6c59c654d61
Andrew Chen <aandrewc@ucla.edu>
2020-05-07 00:11:54 -0700

    preliminary word comparison

--- test_embeddings.py
+++ test_embeddings.py
@@ -1,12 +1,16 @@
 from embeddings import GloveEmbedding, FastTextEmbedding, KazumaCharEmbedding, ConcatEmbedding
+import numpy as np
 
 g = GloveEmbedding('common_crawl_840', d_emb=300, show_progress=True)
-#f = FastTextEmbedding()
 k = KazumaCharEmbedding()
-#c = ConcatEmbedding([g, k])
-for w in ['canada', 'vancouver', 'toronto']:
+
+for w in ['metal1', 'm1', 'METAL1']:
     print('embedding {}'.format(w))
     print(g.emb(w))
-#    print(f.emb(w))
     print(k.emb(w))
-#    print(c.emb(w))
+
+diff1 = np.array(k.emb('metal1')) - np.array(k.emb('METAL1'))
+diff2 = np.array(k.emb('metal1')) - np.array(k.emb('layer'))
+
+print(np.abs(np.mean(diff1)))
+print(np.abs(np.mean(diff2)))
