e61ac1f8011a6c57e7af664d1e1b80855329d647
Andrew Chen <aandrewc@ucla.edu>
2020-05-12 23:09:37 -0700

    match layers names using fuzzy matching

--- test_fuzzy.py
+++ test_fuzzy.py
@@ -1,6 +1,8 @@
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
+from read_csv import read_csv, read_rul
+
 # ratio
 # partial_ratio
 # token_sort_ratio
@@ -18,13 +20,60 @@ from fuzzywuzzy import process
 # out = fuzz.partial_ratio("METALSMG", "MGn")
 # print(out)
 
-layers = ['NW', 'ACTIVE', 'METAL1', 'MINTn', 'VSMGn', 'MSMGn', 'MGn', 'VGn', 'GATE', 'VTL/VTH']
 scorer = fuzz.ratio
-print(scorer)
-print(process.extract("METALINT", layers, limit=4, scorer=scorer))
-print(process.extract("METALSMG", layers, limit=4, scorer=scorer))
-print(process.extract("METAL1", layers, limit=4, scorer=scorer))
-print(process.extract("METALG", layers, limit=4, scorer=scorer))
-print(process.extract("ACTIVE", layers, limit=4, scorer=scorer))
-print(process.extract("VIA1", layers, limit=4, scorer=scorer))
-print(process.extract("VIA[2-3]", layers, limit=4, scorer=scorer))
+
+pdk15_csv = read_csv("calibreDRC_15.csv")
+pdk45_csv = read_csv("calibreDRC_45.csv")
+
+pdk15_rul = read_rul("calibreDRC_15.rul")
+pdk45_rul = read_rul("calibreDRC_45.rul")
+
+layers45 = []
+for key in pdk45_csv.keys():
+    if key != "name":
+        layers45.append(key.split('LAYER')[0])
+
+layers15 = []
+for key in pdk15_csv.keys():
+    if key != "name":
+        layers15.append(key.split('LAYER')[0])
+
+final_pairs = []
+list1 = layers45
+list2 = layers15
+while True:
+    pairs = []
+    for key in list1:
+        result = process.extract(key, list2, limit=2, scorer=scorer)
+        match = result[0][0]
+        score = result[0][1]
+        pairs.append( (key, match, score) )
+        # print(key, ':', match, score)
+
+    max_score = 50
+    perfect_match = False
+    best_pair = []
+    for pair in pairs:
+        key, match, score = pair
+        if score == 100:
+            perfect_match = True
+            list1.remove(key)
+            list2.remove(match)
+            print("matching", key, match, score)
+            final_pairs.append( (key,match) )
+        elif score > max_score:
+            max_score = score
+            best_pair = [key, match]
+    
+    if not best_pair:
+        break
+    elif perfect_match:
+        continue
+    else:
+        key, match = best_pair
+        list1.remove(key)
+        list2.remove(match)
+        final_pairs.append( (key,match) )
+        print("matching", key, match, max_score)
+
+print(final_pairs)
